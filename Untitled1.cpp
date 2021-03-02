a56e08bbe6d51b3169985c0a7da7872569736cd1
Hyungki-Kim <79901765+Hyungki-Kim@users.noreply.github.com>
2021-03-02 17:33:13 +0900

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+main(){
+	
+	printf("Good Morning.\n");
+	printf("Hello hulklee\n");
+	
+	
+	printf("내 이름은요~ %s 이구요\n\n","홍길동" );
+	printf("제 나이는요~ %d 입니다.",9);
+	
+	printf("\\a : 경고음 발생\n");
+	printf("%d%d\n",123,456);
+	printf("%d____%d____%d\n",123,456,789);
+	
+	printf("data type 별 byte 수(int) : %d byte\n",sizeof(int));
+	printf("data type 별 byte 수(char) : %d byte\n",sizeof(char));
+	printf("data type 별 byte 수(float) : %d byte\n",sizeof(float));
+	printf("data type 별 byte 수(double) : %d byte\n",sizeof(double));
+	printf("data type 별 byte 수(long) : %d byte\n",sizeof(long int));
+	
+}
